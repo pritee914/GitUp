7d6767b28b431713f87cab2e0952efba50b8df37
pritee914 <126570298+pritee914@users.noreply.github.com>
2023-03-01 12:03:42 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,13 @@
+public class Sbstraction {
+
+	public static void main(String[] args) {
+			Scanner Sc = new Scanner(System.in);
+			int i,n,s;
+			System.out.println("Enter The value");
+			i = Sc.nextInt(); 
+			n = Sc.nextInt();
+			s = i - n;
+			System.out.println("Substraction " +s);
+	}
+
+}
